ba81af4b517fe2454e44966d09149288abdb8258
Dawid Kozakiewicz <35470291+dawidkozakiewicz@users.noreply.github.com>
2019-09-11 08:48:55 +0200

    add updated URL

--- script.js
+++ script.js
@@ -1,54 +1,32 @@
-/* dzięki temu nie będę musiał zawieszać strony na github pages, albo gdziekolwiek bo: "This API enables cross-origin requests to anywhere oraz:
- Create a request to <url>, and includes CORS headers in the response." */
-var prefix = "https://cors-anywhere.herokuapp.com/"; 
+$(document).ready(function () {
+  getQuote();
+
+  $('.trigger').click(function () {
+    getQuote();
+  });
+});
 
 var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
 
 function getQuote() {
-  /* pobierze nam losowy cytat za pomocą API */
-  $.ajax({
-    type: "GET",
-    url: prefix + quoteUrl,
-    success: createTweet});
-  /* aby nasze zapytania się nie cache'owały. */
-  $.ajaxSetup({
-    cache: false
-  });
+  $.getJSON(quoteUrl, createTweet);
 }
 
 function createTweet(input) {
-console.log(input);
-  var data = input[0];
-/* pod kluczem data.content jest zwykły kod HTML paragrafu. 
-Nie jest to format danych, o jaki nam chodzi.
-W związku z tym za pomocą jQuery tworzymy element HTML na podstawie HTML z tego klucza,
-a następnie wyciągamy z niego zawartość tekstową za pomocą metody .text()
-Wykorzystaliśmy też metodę .trim(), która pozwoli nam 'uciąć' niepotrzebne spacje na początku/końcu stringa: */
-  var quoteText = $(data.content).text().trim();
-
-  var quoteAuthor = data.title;
-
-  if (!quoteAuthor.length) {
-    quoteAuthor = "Unknown author";
+  if (!input.quoteAuthor.length) {
+    input.quoteAuthor = "Unknown Author";
   }
 
-  var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+  var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
+
   if (tweetText.length > 140) {
     getQuote();
-  } else {
+  }
+  else {
     var tweet = tweetLink + encodeURIComponent(tweetText);
-/* jQuery wkleja nam cytat i autora na stronę oraz dokleja do linku całość, żeby mógł się wpisać na twitterze do okienka tweeta: */
-    $('.quote').text(quoteText);
-    $('.author').text("Author: " + quoteAuthor);
+    $('.quote').text(input.quoteText);
+    $('.author').text("Author: " + input.quoteAuthor);
     $('.tweet').attr('href', tweet);
   }
 }
-/* ładuje zaraz po starcie pierwszy losowy cytat i czeka w pogotowiu, aż naciśniemy button, żeby wylosować następny: */
-$(document).ready(function() {
-  getQuote();
-  $('.trigger').click(function() {
-    getQuote();
-    console.log('a ku ku');
-  });
-});
